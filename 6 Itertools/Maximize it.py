6ef8f6283424d988cf3ee7ad8a318388f822725d
S_BHARADWAJ_CAPG2022 <83962241+codester4u@users.noreply.github.com>
2022-03-17 08:29:06 +0530

    Delete 6 Itertools directory

--- 6 Itertools/Maximize it.py
+++
@@ -1,9 +0,0 @@
-from itertools import product
-K,M=list(map(int,input().split()))
-l1=[]
-for i in range(K):
-    x=[abs(int(i)) for i in input().split()][1:]
-    #print(x)
-    l1.append(x)
-results = map(lambda x: sum(i**2 for i in x)%M, product(*l1))
-print(max(results))
